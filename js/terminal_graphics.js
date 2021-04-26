73ac805b0c63aa46c4737c2cfc872fa011e0f4ac
kumaran <kumaran@localhost.localdomain>
2021-04-26 19:53:41 +0530

    terminal page done...almost

--- js/terminal_graphics.js
+++ js/terminal_graphics.js
@@ -8,23 +8,73 @@ cmb_disp();
 
 function cmb_disp() {
     var canvas = document.getElementById('cmb_viewport');
-    var width = canvas.offsetWidth;
+    var width = canvas.offsetWidth - 5;
     console.log(width);
-    var height = canvas.offsetHeight;
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+    var height = canvas.offsetHeight - 5;
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
     renderer.setSize(width, height);
     canvas.appendChild(renderer.domElement);
 
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000)
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+
+
+    function add_sphere() {
+        const sphere_geom = new THREE.SphereGeometry(3, 32, 32);
+        const sphere_mat = new THREE.MeshStandardMaterial({ color: 0xffffff });
+        const sphere = new THREE.Mesh(sphere_geom, sphere_mat);
+        scene.add(sphere);
+    }
+    add_sphere();
+
+
+
+    var loader = new GLTFLoader();
+    loader.load(
+        "/cmb.glb",
+        function (gltf) {
+            var scale = 5.6;
+            bus.body = gltf.scene.children[0];
+            bus.body.name = 'body';
+            bus.body.rotation.set(0, -1.5708, 0);
+            bus.body.scale.set(scale, scale, scale);
+            bus.body.position.set(0, 3.6, 0);
+            bus.body.castShadow = true;
+            bus.frame.add(bus.body);
+        },
+    ); 
+    scene.add(bus.frame)
+
+
+
+
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+    scene.add(ambientLight)
+
+    var pointLight = new THREE.PointLight(0xffffff, 1);
+    pointLight.position.set(25, 50, 25);
+    scene.add(pointLight);
+
+
+    renderer.render(scene, camera)
+
+
+    camera.position.z = 10
+
+
+
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.maxPolarAngle = 0.9 * Math.PI / 2;
+    controls.enableZoom = true;
 
 
 
     window.addEventListener('resize', () => {
         console.log('updating');
-        let width = document.getElementById('cmb_viewport').offsetWidth;
-        let height = document.getElementById('cmb_viewport').offsetHeight;
+        let width = document.getElementById('cmb_viewport').offsetWidth - 5;
+        let height = document.getElementById('cmb_viewport').offsetHeight - 5;
         renderer.setSize(width, height)
         camera.left = width / - 2;
         camera.right = width / 2;
