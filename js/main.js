09b9ed1dccded65b8520de95d9675610d0cb20bb
kumaran <kumaran@localhost.localdomain>
2021-04-27 14:11:00 +0530

    close to release

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 //variables needed for interpolation
-total_age_of_universe=13600000000;
-age_where_we_are=13600000000;
+var total_age_of_universe=13600000000;
+var age_where_we_are=13600000000;
+let ring ;
